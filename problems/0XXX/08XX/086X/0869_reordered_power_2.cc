dafaa3574ca5bc726e93f67cf5f99a21dff2eb61
Mohan Karthik <nemesis.nitt@gmail.com>
2022-08-26 12:32:55 +0530

    Added consts

--- problems/0XXX/08XX/086X/0869_reordered_power_2.cc
+++ problems/0XXX/08XX/086X/0869_reordered_power_2.cc
@@ -3,15 +3,15 @@
 class Solution {
 public:
     bool reorderedPowerOf2(int n) {
-        string n_str = to_string(n);
+        const string n_str = to_string(n);
         sort(n_str.begin(), n_str.end());
         for (int i = 0; i < 32; i++) {
-            string temp = to_string(1<<i);
-            if (temp.length() != n_str.length())
-                continue;
-            sort(temp.begin(), temp.end());
-            if (temp == n_str)
-                return true;
+            const string temp = to_string(1<<i);
+            if (temp.length() == n_str.length()) {
+                sort(temp.begin(), temp.end());
+                if (temp == n_str)
+                    return true;
+            }
         }
         return false;
     }
