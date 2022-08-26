c7c7e9c505d4199e13388278f2d579187491c919
Mohan Karthik <nemesis.nitt@gmail.com>
2022-08-26 12:31:56 +0530

    Minor optimization

--- problems/0XXX/08XX/086X/0869_reordered_power_2.cc
+++ problems/0XXX/08XX/086X/0869_reordered_power_2.cc
@@ -7,6 +7,8 @@ public:
         sort(n_str.begin(), n_str.end());
         for (int i = 0; i < 32; i++) {
             string temp = to_string(1<<i);
+            if (temp.length() != n_str.length())
+                continue;
             sort(temp.begin(), temp.end());
             if (temp == n_str)
                 return true;
